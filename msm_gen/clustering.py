53d06cb760ba860dfccdf6f9e348e12b765dc1d9
Maxwell I. Zimmerman <mizimmer@wustl.edu>
2019-05-18 20:47:58 -0500

    absolute paths used when checking filenames

--- msm_gen/clustering.py
+++ msm_gen/clustering.py
@@ -51,7 +51,10 @@ def load_trjs(trj_filenames, n_procs=1, **kwargs):
             'waiting on nfs. missing %d files (%0.2f s)' % \
             (trj_filenames.shape[0]-trj_filenames_test.shape[0], t1-t0))
         time.sleep(15)
-        trj_filenames_test = np.sort(np.array(glob.glob("trajectories/*.xtc")))
+        trj_filenames_test = np.array(
+            [
+                os.path.abspath(f) 
+                for f in np.sort(np.array(glob.glob("trajectories/*.xtc")))])
         diffs = np.setdiff1d(trj_filenames, trj_filenames_test)
     # parallelize load with **kwargs
     partial_load = partial(md.load, **kwargs)
