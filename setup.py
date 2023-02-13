abd3bff446892d0b7eab5264655bafd2740b81a6
Maxwell Zimmerman <mizimmer@wustl.edu>
2023-02-13 12:22:00 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='fast',version='1.0',
+    description='Implementation of various adaptive sampling techniques',
+    author='Maxwell I. Zimmerman')
+
